d9bb385c40cb2b144fed892a4be966e49490c8da
Turki Almutairi <83261110+Turki20@users.noreply.github.com>
2025-08-28 21:04:28 +0300

    Delete websites/apps.py

--- websites/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class WebsitesConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "websites"          
-    verbose_name = "Websites"
-
-    def ready(self):
-        import websites.signals  
